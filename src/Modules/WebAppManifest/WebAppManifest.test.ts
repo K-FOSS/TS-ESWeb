eaf76d4a75c2f7ccea94a0d11db53d2ebfc7f689
KristianFJones <me@kristianjones.dev>
2021-02-09 05:50:29 +0000

    tests(WebAppManifest): Fix WebAppManifest testing

--- src/Modules/WebAppManifest/WebAppManifest.test.ts
+++ src/Modules/WebAppManifest/WebAppManifest.test.ts
@@ -1,10 +1,12 @@
 // src/Modules/WebAppManifest/WebAppManifest.test.ts
 import { TestSuite } from '@k-foss/ts-estests';
+import { deepStrictEqual } from 'assert';
+import { classToPlainFromExist, plainToClass } from 'class-transformer';
+import { createFastifyTestServer } from '../../Library/Fastify';
+import { logger } from '../../Library/Logger';
+import '../../Utils/Setup';
 import { WebAppManifest } from './WebAppManifest';
 import { WebAppManfiestController } from './WebAppManifestController';
-import { createFastifyTestServer } from '../../Library/Fastify';
-import '../../Utils/Setup';
-import { deepStrictEqual } from 'assert';
 
 export class WebAppManifestTestSuite extends TestSuite {
   public testName = 'Web App Manifest Test Suite';
@@ -12,6 +14,19 @@ export class WebAppManifestTestSuite extends TestSuite {
   public async test(): Promise<void> {
     const manifest: WebAppManifest = {
       name: 'HelloWorld',
+      backgroundColor: '#FFFFFF',
+      description: 'Hello World App1',
+      display: 'standalone',
+      shortName: 'Hello',
+      startURL: '/Test',
+      icons: [
+        {
+          src:
+            'https://www.shareicon.net/data/512x512/2016/07/10/119930_google_512x512.png',
+          type: 'image/png',
+          sizes: '512x512',
+        },
+      ],
     };
 
     await WebAppManfiestController.loadManifest(manifest);
@@ -31,9 +46,11 @@ export class WebAppManifestTestSuite extends TestSuite {
       `GET Request to /WebManifest returns status code 200 with valid loaded Manifest`,
     );
 
+    logger.debug(classToPlainFromExist(manifest, manifestRequest));
+
     deepStrictEqual(
-      manifestRequest.json(),
-      manifest,
+      plainToClass(WebAppManifest, manifestRequest.json()),
+      plainToClass(WebAppManifest, manifest),
       `GET Request to /WebManifest returns same results as loaded manifest`,
     );
   }
